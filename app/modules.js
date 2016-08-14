4c4bd5d28cd25e041ae9d8d214171c787a29a8fb
Rohan Doshi <rrohan92@gmail.com>
2016-08-13 23:29:27 -0400

    modules test failing

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-  	
+  	return (function(){
+  		var name = str2;
+  		var greeting = str1; 
+
+  		function sayIt(){
+  		  return greeting + ', ' + name; 
+  		}
+
+  		return {
+  			name: name,
+  			greeting: greeting,
+  			sayIt: sayIt
+  		};
+  	})();
   }
 };
